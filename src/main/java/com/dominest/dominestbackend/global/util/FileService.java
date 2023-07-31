e33a472eecf9c1fb596d0a5a00fbf0803028e55b
vact19 <chj6703@gmail.com>
2023-07-31 15:53:03 +0900

    Update: FileService 개선
    1. enum FilePrefix 추가하여 파일 저장경로를 선택할 수 있다.
    2. save() 메서드 오버로딩으로 편의성 개선

--- src/main/java/com/dominest/dominestbackend/global/util/FileService.java
+++ src/main/java/com/dominest/dominestbackend/global/util/FileService.java
@@ -2,6 +2,7 @@ package com.dominest.dominestbackend.global.util;
 
 import com.dominest.dominestbackend.global.exception.ErrorCode;
 import com.dominest.dominestbackend.global.exception.exceptions.file.FileIOException;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,31 +20,42 @@ import java.util.UUID;
 @Service
 public class FileService {
     @Value("${file.upload.path}")// yml 설정파일
-    private String fileStorePrefix;
+    private String fileUploadPath;
 
     // return fullFilePath
-    public List<String> saveAll(List<MultipartFile> multipartFiles){
+    public List<String> save(List<MultipartFile> multipartFiles, FilePrefix prefix){
         List<String> storedFilePaths = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFiles) {
-            // empty Check. type=file 이며 name이 일치한다면, 본문이 비어있어도 MultiPartFile 객체가 생성된다.
-            if (multipartFile.isEmpty()){
-                continue;
-            }
-            String originalFileName = multipartFile.getOriginalFilename();
-            String storedFilePath = createStoredFilePath(originalFileName);
-
-            try {
-                multipartFile.transferTo(new File(fileStorePrefix + storedFilePath));
-            } catch (IOException e) {
-                // FileIOException 발생시키기 전에, IOEXCEPTION 에 대한 로그를 남긴다.
-                log.error("IOEXCEPTION: " + originalFileName + " 저장 불가" );
-                e.printStackTrace();
-                throw new FileIOException(ErrorCode.FILE_CANNOT_BE_STORED);
-            }
+            String storedFilePath = save(multipartFile, prefix);
+            if (storedFilePath == null) continue;
             storedFilePaths.add(storedFilePath);
         }
         // 저장한 파일의 경로 리스트를 반환한다.
         return storedFilePaths;
+    }
+
+    /**
+     * @return "prefix" + "저장된 파일명 UUID" + ".확장자"
+     */
+    // return fullFilePath
+    public String save(MultipartFile multipartFile, FilePrefix prefix){
+        // empty Check. type=file 이며 name이 일치한다면, 본문이 비어있어도 MultiPartFile 객체가 생성된다.
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+        String originalFileName = multipartFile.getOriginalFilename();
+        String storedFilePath = prefix.getPrefix() + createStoredFilePath(originalFileName);
+
+        try {
+            multipartFile.transferTo(new File(fileUploadPath + storedFilePath));
+        } catch (IOException e) {
+            // FileIOException 발생시키기 전에, IOEXCEPTION 에 대한 로그를 남긴다.
+            log.error("IOEXCEPTION: " + originalFileName + " 저장 불가" );
+            e.printStackTrace();
+            throw new FileIOException(ErrorCode.FILE_CANNOT_BE_STORED);
+        }
+        // 저장한 파일의 경로 리스트를 반환한다.
+        return storedFilePath;
     }
 
     private String createStoredFilePath(String originalFileName) {
@@ -66,11 +78,26 @@ public class FileService {
     }
 
     public byte[] getByteArr(String fileName) {
-        try (InputStream imageStream = new FileInputStream(fileStorePrefix + fileName)) {
+        try (InputStream imageStream = new FileInputStream(fileUploadPath + fileName)) {
             return imageStream.readAllBytes();
         } catch (IOException e) {
             e.printStackTrace();
             throw new FileIOException(ErrorCode.FILE_CANNOT_BE_READ);
+        }
+    }
+
+    // fileUploadPath 내부에 저장될 directory 를 선택한다.
+    // fileUplaodPath + FilePrefix + fileName 으로 저장된다.
+    @Getter
+    public enum FilePrefix {
+        RESIDENT_PDF("resident/pdf/"),
+        NONE(""),
+        ;
+
+        private final String prefix;
+
+        FilePrefix(String prefix) {
+            this.prefix = prefix;
         }
     }
 }
