0492483b2ad5825148f3fc448bd2560965c413d4
vact19 <chj6703@gmail.com>
2023-08-16 01:27:43 +0900

    Update: AuditorAwareImpl 인증정보 없을 시 unknown 반환

--- src/main/java/com/dominest/dominestbackend/global/config/jpa/AuditorAwareImpl.java
+++ src/main/java/com/dominest/dominestbackend/global/config/jpa/AuditorAwareImpl.java
@@ -4,6 +4,7 @@ import com.dominest.dominestbackend.domain.jwt.service.TokenManager;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +22,10 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
         // SecurityContext 에서 Authentication 객체를 꺼내서 email을 꺼내서 리턴
-        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String principal =
+                authentication == null ? "unknown"
+                : (String) authentication.getPrincipal();
 
 //        //  1. authorization 필수 체크. 헤더 부분에 Authorization 이 없으면 지정한 예외를 발생시킴
 //        //  토큰 유무 확인
