4b3858d1d0d397b9d21fddbd541bb9f788742e01
vact19 <chj6703@gmail.com>
2023-08-17 02:37:46 +0900

    Update: jjwt 라이브러리 최신화, 필터 리팩터링

--- src/main/java/com/dominest/dominestbackend/global/exception/ErrorCode.java
+++ src/main/java/com/dominest/dominestbackend/global/exception/ErrorCode.java
@@ -17,9 +17,10 @@ public enum ErrorCode {
     // 인증 - 토큰
     NOT_EXISTS_AUTHORIZATION(401, "Authorization Header가 빈 값입니다."),
     NOT_VALID_BEARER_GRANT_TYPE(401, "인증 타입이 Bearer 타입이 아닙니다."),
-    ACCESS_TOKEN_EXPIRED(401, "해당 access token은 만료됐습니다."),
+    TOKEN_EXPIRED(401, "해당 token은 만료되었습니다."),
+    ACCESS_TOKEN_EXPIRED(401, "해당 access token은 만료되었습니다."),
     NOT_ACCESS_TOKEN_TYPE(401, "tokenType이 access token이 아닙니다."),
-    REFRESH_TOKEN_EXPIRED(401, "해당 refresh token은 만료됐습니다."),
+    REFRESH_TOKEN_EXPIRED(401, "해당 refresh token은 만료되었습니다."),
     REFRESH_TOKEN_NOT_FOUND(400, "해당 refresh token은 존재하지 않습니다."),
     NOT_VALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     NO_ACCESS_USER(403, "권한이 없습니다."),
