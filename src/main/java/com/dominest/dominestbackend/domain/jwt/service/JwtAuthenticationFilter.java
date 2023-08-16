528a6b69a3af846d6aa828893d2a0d413991c230
vact19 <chj6703@gmail.com>
2023-08-17 03:22:46 +0900

    Fix: JwtFilter 오류 수정
    permitAll()은 필터 미적용이 아닌, 필터 적용 후 인증정보가 없어도 요청을 허용하는 것.
    필터 로직의 예외상황에서 throw Exception을 하지 않고, filterChain.doFilter()로 넘기도록 함.

--- src/main/java/com/dominest/dominestbackend/domain/jwt/service/JwtAuthenticationFilter.java
+++ src/main/java/com/dominest/dominestbackend/domain/jwt/service/JwtAuthenticationFilter.java
@@ -1,7 +1,5 @@
 package com.dominest.dominestbackend.domain.jwt.service;
 
-import com.dominest.dominestbackend.global.exception.ErrorCode;
-import com.dominest.dominestbackend.global.exception.exceptions.auth.NotValidTokenException;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -27,28 +26,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 토큰 정보 가져오기
         String token = resolveToken(request);
 
-        if (! StringUtils.hasText(token)) { // 토큰이 존재하면
-            throw new NotValidTokenException(ErrorCode.NOT_VALID_TOKEN);
+        if (StringUtils.hasText(token) && tokenManager.validateToken(token)) { // 토큰이 존재하고 유효한 경우
+            Claims claims = tokenManager.getTokenClaims(token);
+
+            String email = tokenManager.getMemberEmail(token);
+//                String tokenType = tokenManager.getTokenType(token);
+
+            // 토큰 유형별로 부여할 권한 설정
+            // List<GrantedAuthority> authorities;
+            // if (tokenType.equalsIgnoreCase("ACCESS")) {
+            //     authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+            // } else {
+            //     authorities = Collections.emptyList();
+            // }
+
+            if (! tokenManager.isTokenExpired(claims.getExpiration())) {
+                // 인증 정보 저장
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
-        Claims claims = tokenManager.getTokenClaims(token);
-
-        if (tokenManager.isTokenExpired(claims.getExpiration())) {
-            throw new NotValidTokenException(ErrorCode.TOKEN_EXPIRED);
-        }
-        String email = tokenManager.getMemberEmail(token);
-
-//        String tokenType = tokenManager.getTokenType(token);
-        // 토큰 유형별로 부여할 권한 설정
-        // List<GrantedAuthority> authorities;
-        // if (tokenType.equalsIgnoreCase("ACCESS")) {
-        //     authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-        // } else {
-        //     authorities = Collections.emptyList();
-        // }
-
-        // 인증 정보 저장
-        Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         // 다음 필터로 전달
         filterChain.doFilter(request, response);
