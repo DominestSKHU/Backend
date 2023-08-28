0f9da514fda980d6f009605c09c4d8c15a1f8fe0
vact19 <chj6703@gmail.com>
2023-08-28 17:14:39 +0900

    Fix: 처음 등록한 즐겨찾기 TRUE상태로 변경

--- src/main/java/com/dominest/dominestbackend/domain/favorite/FavoriteService.java
+++ src/main/java/com/dominest/dominestbackend/domain/favorite/FavoriteService.java
@@ -23,15 +23,17 @@ public class FavoriteService {
     public boolean addOrUndo(Long categoryId, String userEmail) {
         // 일단 조인해. 없으면 예외처리해.
         Favorite favorite = favoriteRepository.findByCategoryIdAndUserEmail(categoryId, userEmail);
+
         if (favorite == null) {
             User user = userService.getUserByEmail(userEmail);
             Category category = categoryService.getCategoryById(categoryId);
+
             Favorite newFavorite = Favorite.builder()
                     .user(user)
                     .category(category)
                     .build();
             favoriteRepository.save(newFavorite);
-            return newFavorite.isOnOff();
+            return true;  // 추가되었으므로 add를 의미하는 true 반환
         }
 
         return favorite.switchOnOff();
