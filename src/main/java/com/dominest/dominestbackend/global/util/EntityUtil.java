747563c8f4d2753e4d3bced326eec46cf8c92055
vact19 <chj6703@gmail.com>
2023-08-30 14:11:05 +0900

    Rename: EntityUtil.checkNotFound -> mustNotNull

--- src/main/java/com/dominest/dominestbackend/global/util/EntityUtil.java
+++ src/main/java/com/dominest/dominestbackend/global/util/EntityUtil.java
@@ -3,22 +3,23 @@ package com.dominest.dominestbackend.global.util;
 
 import com.dominest.dominestbackend.global.exception.ErrorCode;
 import com.dominest.dominestbackend.global.exception.exceptions.BusinessException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.Optional;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EntityUtil {
-    public static <E> E checkNotFound(E entity, ErrorCode errorCode) {
+    public static <T> T mustNotNull(T entity, ErrorCode errorCode) {
         if (entity == null) throw new BusinessException(errorCode);
         return entity;
     }
 
-    public static <E> E checkNotFound(Optional<E> entity, ErrorCode errorCode) {
-        return entity.orElseThrow(() -> new BusinessException(errorCode));
+    public static <T> T mustNotNull(Optional<T> optEntity, ErrorCode errorCode) {
+        return optEntity.orElseThrow(() -> new BusinessException(errorCode));
     }
 
     public static void mustNull(Object entity, ErrorCode errorCode) {
         if (entity != null) throw new BusinessException(errorCode);
     }
-
-
 }
