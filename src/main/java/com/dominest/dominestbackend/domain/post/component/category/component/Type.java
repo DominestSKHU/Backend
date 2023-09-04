dbf9c16fc973d23861527e9879c83ea0d4ee4e40
vact19 <chj6703@gmail.com>
2023-09-04 15:36:34 +0900

    Update: categoryLink 수정
    undelivered-parcel-registers -> undelivered-parcel

--- src/main/java/com/dominest/dominestbackend/domain/post/component/category/component/Type.java
+++ src/main/java/com/dominest/dominestbackend/domain/post/component/category/component/Type.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum Type {
     IMAGE("image-types"), TEXT_IMAGE("text-image-types")
-    , UNDELIVERED_PARCEL_REGISTER("undelivered-parcel-registers")
+    , UNDELIVERED_PARCEL_REGISTER("undelivered-parcel")
     ;
 
     private final String url;
