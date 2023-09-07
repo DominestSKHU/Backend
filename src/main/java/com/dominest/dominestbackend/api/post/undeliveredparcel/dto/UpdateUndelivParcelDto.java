8a6286314e2a48de1996f71bc8fcdfc60fb825a0
vact19 <chj6703@gmail.com>
2023-09-07 15:59:32 +0900

    Update. 미수령택배 생성시 null -> empty string으로 변환

--- src/main/java/com/dominest/dominestbackend/api/post/undeliveredparcel/dto/UpdateUndelivParcelDto.java
+++ src/main/java/com/dominest/dominestbackend/api/post/undeliveredparcel/dto/UpdateUndelivParcelDto.java
@@ -16,11 +16,11 @@ public class UpdateUndelivParcelDto {
     @Getter
     public static class Req {
         @Length(max = 50, message = "수취인 이름은 50자를 넘을 수 없습니다")
-        String recipientName;
+        String recipientName = "";
         @Length(max = 50, message = "수취인 전화번호는 50자를 넘을 수 없습니다")
-        String recipientPhoneNum;
+        String recipientPhoneNum = "";
         @Length(max = 500, message = "처리내용 설명은 500자를 넘을 수 없습니다")
-        String instruction;
+        String instruction = "";
         @NotNull(message = "처리상태는 비어있을 수 없습니다")
         UndeliveredParcel.ProcessState processState;
     }
