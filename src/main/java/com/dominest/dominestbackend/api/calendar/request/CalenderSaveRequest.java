7371d2a8fc53c24fe01a1d1a19ec97cc01c7d26d
jiyoungmerong <jiyoungmerong@gmail.com>
2023-09-14 15:32:25 +0900

    feat: 캘린더 저장 request

---
+++ src/main/java/com/dominest/dominestbackend/api/calendar/request/CalenderSaveRequest.java
@@ -0,0 +1,15 @@
+package com.dominest.dominestbackend.api.calendar.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CalenderSaveRequest {
+    private LocalDate date; // 날짜
+
+    private String content; // 내용 작성
+
+}
