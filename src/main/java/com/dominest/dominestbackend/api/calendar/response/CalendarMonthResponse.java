2994622821242e285b80f4269ae82cd3ae68af99
jiyoungmerong <jiyoungmerong@gmail.com>
2023-10-24 22:57:57 +0900

    feat: 월별 스케줄 가져오기 로직 추가

---
+++ src/main/java/com/dominest/dominestbackend/api/calendar/response/CalendarMonthResponse.java
@@ -0,0 +1,14 @@
+package com.dominest.dominestbackend.api.calendar.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class CalendarMonthResponse {
+    private int id;
+    private boolean content; // 내용 유무
+}
