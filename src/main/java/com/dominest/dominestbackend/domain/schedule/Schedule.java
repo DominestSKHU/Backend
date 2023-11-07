b227c829453b4e8deed3ad642009cd62af945627
jiyoungmerong <jiyoungmerong@gmail.com>
2023-11-07 21:40:25 +0900

    chore: 요일 내부 enum 추가

--- src/main/java/com/dominest/dominestbackend/domain/schedule/Schedule.java
+++ src/main/java/com/dominest/dominestbackend/domain/schedule/Schedule.java
@@ -16,12 +16,26 @@ public class Schedule {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String dayOfWeek; // 요일
+    public enum Weekday {
+        월요일, 화요일, 수요일, 목요일, 금요일;
+
+        public static Weekday fromString(String value) {
+            for (Weekday weekday : Weekday.values()) {
+                if (weekday.name().equals(value)) {
+                    return weekday;
+                }
+            }
+            throw new IllegalArgumentException("Invalid weekday value: " + value);
+        }
+    }
+
+    @Enumerated(EnumType.STRING)
+    private Weekday dayOfWeek; // 요일
 
     private String timeSlot;  // 시간
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "Schedule_Usernames", joinColumns = @JoinColumn(name = "id"))
+    @ElementCollection
+    @CollectionTable(name = "Schedule_Usernames", joinColumns = @JoinColumn(name = "schedule_id"))
     @Column(name = "username")
     private List<String> usernames;
 
