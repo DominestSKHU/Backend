72b2dcf774b96499632c8dd56f26c877117f4fc8
vact19 <chj6703@gmail.com>
2023-12-06 20:40:37 +0900

    Add. ConstraintViolationException에 대한 예외 핸들러 추가

--- src/main/java/com/dominest/dominestbackend/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dominest/dominestbackend/global/exception/handler/GlobalExceptionHandler.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @RestControllerAdvice
@@ -48,7 +51,7 @@ public class GlobalExceptionHandler {
             sb.setLength(0);
         }
 
-        return createErrorResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, errorInfoMap);
+        return createErrorResponse(HttpStatus.BAD_REQUEST, errorInfoMap);
     }
 
     /** @RequestParam 파라미터 누락*/
@@ -56,14 +59,36 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseDto<String>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e, HttpServletRequest request) {
         printLog(e, request);
         String message = "파라미터 '" + e.getParameterName() + "'이 누락되었습니다.";
-        return createErrorResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, message);
+        return createErrorResponse(HttpStatus.BAD_REQUEST, message);
     }
 
-    // BusinessException 을 상속한 다른 Custom Exception 에도 적용된다.
+    /** @Valid 파라미터 누락*/
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponseDto<Map<String, String>>> handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
+        printLog(e, request);
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+
+        StringBuilder sb = new StringBuilder();
+        Map<String, String> errorInfoMap = new HashMap<>();
+        for (ConstraintViolation constraintViolation : constraintViolations) {
+            String errorMsg = sb
+                    .append(constraintViolation.getMessage())
+                    .append(". 입력값: ")
+                    .append(constraintViolation.getInvalidValue())
+                    .toString();
+
+            errorInfoMap.put(constraintViolation.getConstraintDescriptor().getAnnotation().annotationType().getSimpleName(), errorMsg);
+
+            sb.setLength(0);
+        }
+
+        return createErrorResponse(HttpStatus.BAD_REQUEST, errorInfoMap);
+    }
+
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<ErrorResponseDto<String>> handleBusinessException(IllegalArgumentException e, HttpServletRequest request){
         printLog(e, request);
-        return createErrorResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage());
+        return createErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     // ENUM 변환실패, 날짜타입에 2999-15-99 와 같은 잘못된 값이 들어올 때 발생
@@ -77,28 +102,27 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({BusinessException.class})
     public ResponseEntity<ErrorResponseDto<String>> handleBusinessException(BusinessException e, HttpServletRequest request){
         printLog(e, request);
-        return createErrorResponse(e.getStatusCode(), e.getHttpStatus(), e.getMessage());
+        return createErrorResponse(e.getHttpStatus(), e.getMessage());
     }
 
     // 비즈니스 로직이 아닌 애플리케이션 서비스 로직상 예외
     @ExceptionHandler({AppServiceException.class})
     public ResponseEntity<ErrorResponseDto<String>> handleAppServiceException(AppServiceException e, HttpServletRequest request){
         printLog(e, request);
-        return createErrorResponse(e.getStatusCode(), e.getHttpStatus(), e.getMessage());
+        return createErrorResponse(e.getHttpStatus(), e.getMessage());
     }
 
     // 예상하지 못한 예외 발생 시, 예외 로그 전체를 서버에 남기고, 로그 자체를 모두 클라이언트에 전송한다.
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto<String>> handleException(Exception e, HttpServletRequest request){
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        log.error("예외처리 범위 외의 오류 발생. " + httpStatus.toString()); // enum.name() enum.toString() 차이 기억하자. name은 단순 문자열변환, toString은 오버라이딩된 메서드 호출
+        log.error("예외처리 범위 외의 오류 발생."); // enum.name() enum.toString() 차이 기억하자. name은 단순 문자열변환, toString은 오버라이딩된 메서드 호출
         printLog(e, request);
 
-        return createErrorResponse(httpStatus.value(), httpStatus, e.getMessage());
+        return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
-    private <T> ResponseEntity<ErrorResponseDto<T>> createErrorResponse(int statusCode, HttpStatus httpStatus, T errorMessage) {
-        ErrorResponseDto<T> errDto = new ErrorResponseDto<>(statusCode, httpStatus, errorMessage);
+    private <T> ResponseEntity<ErrorResponseDto<T>> createErrorResponse(HttpStatus httpStatus, T errorMessage) {
+        ErrorResponseDto<T> errDto = new ErrorResponseDto<>(httpStatus.value(), httpStatus, errorMessage);
         return ResponseEntity.status(httpStatus).body(errDto);
     }
 
