0b5e755dc94092645319941072d29d1a2b703725
Hyeonwoo No <nhwgit@gmail.com>
2023-12-13 10:22:09 +0900

    manual 게시글 조회시 요청한 게시글이 아닌 다른 게시글이 불러와지는 오류 fix

--- src/main/java/com/dominest/dominestbackend/domain/post/manual/ManualPostRepository.java
+++ src/main/java/com/dominest/dominestbackend/domain/post/manual/ManualPostRepository.java
@@ -2,12 +2,10 @@ package com.dominest.dominestbackend.domain.post.manual;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.persistence.Entity;
 
 public interface ManualPostRepository extends JpaRepository<ManualPost, Long> {
 
@@ -16,13 +14,9 @@ public interface ManualPostRepository extends JpaRepository<ManualPost, Long> {
             , countQuery = "SELECT count(*) FROM ManualPost m WHERE m.category.id = :categoryId")
     Page<ManualPost> findAllByCategory(@Param("categoryId") Long categoryId, Pageable pageable);
 
-   // @EntityGraph(attributePaths = {"writer", "attachmentUrls", "imageUrls", "videoUrls"})
-   // @Query(value = "SELECT m FROM ManualPost m")
-   // ManualPost findManualPostIncludeAllColumn(long manualId);
-
-    //@EntityGraph(attributePaths = {"writer", "imageUrls", "attachmentUrls", "videoUrls"})
     @Query(value = "SELECT m FROM ManualPost m left join fetch m.imageUrls left join fetch m.writer " +
-            "left join fetch m.attachmentUrls left join fetch m.videoUrls")
+            "left join fetch m.attachmentUrls left join fetch m.videoUrls" +
+            " WHERE m.id = :manualId" )
     ManualPost findManualPostIncludeAllColumn(long manualId);
     void deleteByCategoryId(Long categoryId);
 }
